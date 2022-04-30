1a9b3847c18ed1e96aabe7fadf1aa18023a93d16
Cezar Amorim <cezar.dba@gmail.com>
2022-04-30 18:50:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const linksSocialMedia = {
+  github: 'cezaramorim',
+  youtube: 'jakelinygracielly',
+  instagram: 'jakeliny.gracielly',
+  facebook: 'maykbrito',
+  twitter: 'jakelinytec'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLinkProfile.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
